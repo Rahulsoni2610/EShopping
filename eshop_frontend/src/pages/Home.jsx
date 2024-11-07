cc320ce2c55078a52db36353a7aa60532e04ad47
Virendra Rathore <virendrarathore6248@gmail.com>
2024-11-07 22:33:14 +0530

    Added navigation and hero section

--- eshop_frontend/src/pages/Home.jsx
+++ eshop_frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navigation from '../Components/Navigation'
+import HeroSection from '../Components/HeroSection'
 import Footer from '../Components/Footer'
 const Home = () => {
   return (
     <div>
       <Navigation />
+      <HeroSection/>
       <Footer />
     </div>
   )
